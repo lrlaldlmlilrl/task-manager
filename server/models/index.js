efc20a3b99a21bb154c805a199d2e47410243437
MirRAd <kamalutdinov2019@list.ru>
2026-06-07 16:37:25 +0500

    Prepare server for production hosting

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,17 @@
 import { DataTypes, Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("postgres://admin:123@localhost:5432/db", {
-logging: false
+const databaseUrl = process.env.DATABASE_URL || "postgres://admin:123@localhost:5432/db";
+
+const sequelize = new Sequelize(databaseUrl, {
+    logging: false,
+    dialectOptions: process.env.NODE_ENV === "production"
+        ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+        : {}
 });
 
 export { sequelize, DataTypes };
